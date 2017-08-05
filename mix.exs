32ac5dfa138d55189042ec59902d623a8de5af74
riacataquian <icataquian@gmail.com>
2017-08-05 13:50:14 +0800

    Adds mix ecto.seed alias

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule FortuneGenerator.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+    ["ecto.setup": ["ecto.create", "ecto.migrate", "ecto.seed"],
+     "ecto.seed": ["run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
      "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
   end
