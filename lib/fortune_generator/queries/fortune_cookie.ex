2fe23e834d5ad57c56c08ddf525f6d2d4a5f639f
riacataquian <icataquian@gmail.com>
2017-08-06 12:28:47 +0800

    Adds moduledoc, doc and typespecs

--- lib/fortune_generator/queries/fortune_cookie.ex
+++ lib/fortune_generator/queries/fortune_cookie.ex
@@ -6,14 +6,55 @@ defmodule FortuneGenerator.Queries.FortuneCookie do
     FortuneCookie
   }
 
+  @moduledoc """
+
+  Module for FortuneCookie queries.
+  Found here are expressions that interacts with FortuneCookie schema.
+
+  """
+
+  @doc """
+
+  Fetches a single record of FortuneCookie in the database.
+
+  ### Example
+
+      iex> FortuneGenerator.Queries.get(1)
+      %FortuneGenerator.FortuneCookie{..}
+
+  """
+  @spec get(integer) :: struct
   def get(id) do
     Repo.get(FortuneCookie, id)
   end
 
+  @doc """
+
+  Returns a changeset for FortuneCookie schema.
+
+  ### Example
+
+      iex> FortuneGenerator.Queries.build_changeset(%FortuneGenerator.FortuneCookie{})
+      %Ecto.Changeset{..}
+
+  """
+  @spec build_changeset(struct, map) :: struct
   def build_changeset(struct, params \\ %{}) do
     FortuneCookie.changeset(struct, params)
   end
 
+  @doc """
+
+  Fetches, build changeset and update a single FortuneCookie record.
+  Returns the updated record.
+
+  ### Example
+
+      iex> FortuneGenerator.Queries.update(1, %{fortune: "Confess and be hanged."})
+      %FortuneGenerator.FortuneCookie{..}
+
+  """
+  @spec update(integer, map) :: struct
   def update(fortune_id, params) do
     fortune_id
     |> get()
@@ -21,6 +62,16 @@ defmodule FortuneGenerator.Queries.FortuneCookie do
     |> Repo.update()
   end
 
+  @doc """
+
+  Randomly retrieve a single FortuneCookie in the database.
+
+  ### Example
+
+      iex> FortuneGenerator.Queries.random()
+      %FortuneGenerator.FortuneCookie{..}
+  """
+  @spec random :: struct
   def random do
     query = from f in FortuneCookie, order_by: fragment("RANDOM()"), limit: 1
 
--- test/support/acceptance_helpers.ex
+++ test/support/acceptance_helpers.ex
@@ -1,12 +1,14 @@
 defmodule FortuneGenerator.AcceptanceHelpers do
   use Wallaby.DSL
 
+  @spec click_reveal_fortune(struct) :: struct
   def click_reveal_fortune(session) do
     session
     |> visit("/")
     |> click_link("Click me to reveal your fortune")
   end
 
+  @spec click_change_fortune(struct) :: struct
   def click_change_fortune(session) do
     session
     |> visit("/")
